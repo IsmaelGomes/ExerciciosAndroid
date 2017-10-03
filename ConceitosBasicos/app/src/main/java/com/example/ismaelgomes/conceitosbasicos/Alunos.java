0b32d72f110ccc36228f7d269644fa1c0ad5ebb5
Ismael Gomes <ismaelgomes800@gmail.com>
2017-10-03 20:43:30 -0300

    Conceitos Básicos: - Refatoração
    Exercício Views:
    - Alteração do tema da segunda activity

--- ConceitosBasicos/app/src/main/java/com/example/ismaelgomes/conceitosbasicos/Alunos.java
+++ ConceitosBasicos/app/src/main/java/com/example/ismaelgomes/conceitosbasicos/Alunos.java
@@ -1,13 +1,11 @@
 package com.example.ismaelgomes.conceitosbasicos;
 
-import android.Manifest;
+
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
